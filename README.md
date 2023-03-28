025dbe4510e6b1e4d3eea05b39e632cd044b182e
EdgarVRP <edgarvrp22@gmail.com>
2023-03-28 08:40:34 -0600

    Se corrigen errores y se comenta en readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Generation-Codigo-de-otros-3
+
  Revision a codigo elaborado por otra persona
+
+Documento HTML
+
+* El link del html al JS es incorrecto
+* El link del html al css falta añadir "./"
+* Se cambia la linea a que sea una lista y el identificador sea un id
+* En linea 15 se añade una clase al input
+
+Documento js
+
+* Linea 11 debe ser get element by Id
+* En la linea 31 se hace llamado a una funcion no existente
+
+Documento CSS
--- filtro.js
+++ filtro.js
@@ -8,8 +8,9 @@ const productos = [
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
 ]
 
-const li = document.getElementsByName("lista-de-productos")
+const li = document.getElementById("lista-de-productos")
 const $i = document.querySelector('.input');
+console.log($i);
 
 for (let i = 0; i < productos.length; i++) {
   var d = document.createElement("div")
@@ -28,7 +29,7 @@ for (let i = 0; i < productos.length; i++) {
   li.appendChild(d)
 }
 
-displayProductos(productos)
+//displayProductos(productos)
 const botonDeFiltro = document.querySelector("button");
 
 botonDeFiltro.onclick = function() {
